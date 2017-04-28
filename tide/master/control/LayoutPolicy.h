068a81d6627d4a8c08ca951011918b20ca323a3b
Nataniel <nataniel.hofer@gmail.com>
2017-04-28 14:59:14 +0200

    review with raphael

--- tide/master/control/LayoutPolicy.h
+++ tide/master/control/LayoutPolicy.h
@@ -2,37 +2,37 @@
 #define LAYOUTPOLICY_H
 #include "types.h"
 
-//Hardcoded variables which are also defined in style.js
-namespace controlSpecifications{
+// Hardcoded variables which are also defined in style.js
+namespace controlSpecifications
+{
 const qreal INSIDE_MARGIN_RELATIVE = 0.02;
 const qreal SIDEBAR_WIDTH_REL_TO_DISPLAYGROUP_HEIGHT = 0.3 * 0.3;
 const qreal WINDOW_CONTROLS_MARGIN_PX = 200.0;
 const qreal WINDOW_SPACING_PX = 80.0;
 const qreal WINDOW_TITLE_HEIGHT = 100.0;
-//TODO check this, could not see it in style.js
+// TODO check this, could not see it in style.js
 const qreal MOVIE_BAR_HEIGHT = 100.0;
 }
 
 /**
- * Abstract class whose derived classes must deal with the layout in focus coordinates
+ * Abstract class whose derived classes must deal with the layout in focus
+ * coordinates
  * @brief The LayoutPolicy class
  */
 class LayoutPolicy
 {
 public:
-    LayoutPolicy( const DisplayGroup& group );
+    LayoutPolicy(const DisplayGroup& group);
 
     /** @return the focused coordinates for the window. */
-    virtual QRectF getFocusedCoord( const ContentWindow& window ) const=0;
+    virtual QRectF getFocusedCoord(const ContentWindow& window) const = 0;
 
     /** Update the focused coordinates for the set of windows. */
-    virtual void updateFocusedCoord( const ContentWindowSet& windows ) const=0;
-    virtual ~LayoutPolicy(){};
-
+    virtual void updateFocusedCoord(const ContentWindowSet& windows) const = 0;
+    virtual ~LayoutPolicy() {}
 protected:
     const DisplayGroup& _group;
     QRectF _getAvailableSpace() const;
-
 };
 
 #endif
